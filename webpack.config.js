4e2d961bbcf1bf55d28236897a8b902caa00a103
Eric Prud'hommeaux <eric@w3.org>
2018-02-22 04:53:56 -0500

    ~ gitignore

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  entry: './doc/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'doc')
+  },
+  node: {
+    fs: 'empty'
+  }
+};
